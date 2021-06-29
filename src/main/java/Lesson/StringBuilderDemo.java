8cbea1b4809e61c47dab2f661245acbdb5adaf99
Renu Jaishankar <Renu.Jaishankar@yahoo.com>
2021-06-29 08:28:34 -0400

    first commit

---
+++ src/main/java/Lesson/StringBuilderDemo.java
@@ -0,0 +1,10 @@
+package Lesson;
+
+public class StringBuilderDemo {
+    public static void main(String[] args){
+        String palindrome = " Dot saw I was Tod";
+        StringBuilder sb = new StringBuilder(palindrome);
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
